4dad9e159b4dfbaf2c170226ffc573432e2fb99e
robotseth <thebonsaidude@gmail.com>
2022-04-14 10:45:53 -0700

    fixed video streaming

--- Final Project/video_test.py
+++ Final Project/video_test.py
@@ -1,8 +1,22 @@
-import cv2
-
-cap = cv2.VideoCapture(0)
-
+import numpy as np
+import cv2 as cv
+cap = cv.VideoCapture(0)
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
 while True:
-    _, img = cap.read()
-    cv2.imshow("Line Detection", img)
-cv2.destroyAllWindows()
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+    # Our operations on the frame come here
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    # Display the resulting frame
+    cv.imshow('frame', gray)
+    if cv.waitKey(1) == ord('q'):
+        break
+# When everything done, release the capture
+cap.release()
+cv.destroyAllWindows()
